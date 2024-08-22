23338adbff6af5f70bbcb33fd5475731eb5026a2
AAro <aaro.se7en@gmail.com>
2024-08-22 20:49:43 +0330

    add onclick to close the sidebar when select a link

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -4,6 +4,7 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import {useState} from "react";
 
 const links = [
   {
@@ -30,8 +31,14 @@ const links = [
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
@@ -52,6 +59,7 @@ const MobileNav = () => {
                 href={link.path}
                 key={index}
                 className={` ${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
+                onClick={handleLinkClick}
               >
                 {link.name}
               </Link>
